f20e868963f31d3c96acc5eafb50c9242b067779
Sakif Hridoy <sakifworkspace@gmail.com>
2022-01-15 20:22:04 +0600

    create object with a variable and access them in console & create functions inside objects commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello')
+// var obj = {};
+// var obj1 = {};
+
+// console.log(obj)
+// console.log(obj1)
+
+var book = {
+    name:'Functional Javascript',
+    author:'Lwis Handshake',
+    publisher :'O\'Really',
+    page:259,
+    print:function(){
+        console.log(this.name,this.author)
+    }
+}
+
+console.log(book)
+book.print()
+
+// console.log('Book Name: '+ book.name);
+// console.log('Auhtor Name: ' + book['author'])// To access object property with bracket notation & property name must be in single or double quotes
