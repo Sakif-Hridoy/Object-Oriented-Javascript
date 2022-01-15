bee81dc25dfb69b39eb889102db1b8a95142071d
Sakif Hridoy <sakifworkspace@gmail.com>
2022-01-15 20:48:33 +0600

    add properties/values in objects with dot and bracket notaion commit

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ book.print()
 
 console.log('Book Name: '+ book.name);
 console.log('Auhtor Name: ' + book['author'])// To access object property with bracket notation & property name must be in single or double quotes
+
+// In Objects,properties could be created with dots
+book.publishedYear = 2010;// Adding properties into book object
+
+console.log('Published Year : ' + book.publishedYear)
+
+// In Objects,properties could be created with bracket notation
+book['price'] = 30;
+console.log('Price: ' + book.price)
+
+// // Now book Object have 7 properties
+// //Print object with values method 1
+// console.log(book)
+
+// //Print object with values using for in loop
+// for (var props in book){
+//     console.log(props)
+//     console.log(props + ' = ' + book.props)//This will return undefined
+//     console.log(props + ' = ' + book[props])//With brakcet notations the problem is solved,so this is Method
+// }
