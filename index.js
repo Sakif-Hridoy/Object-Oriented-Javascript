96efc3d10980b665bddbca1dcfb6bf1132a361c6
Sakif Hridoy <sakifworkspace@gmail.com>
2022-01-19 18:16:39 +0600

    constructor pattern in js commit 1

--- index.js
+++ index.js
@@ -4,38 +4,51 @@
 // console.log(obj)
 // console.log(obj1)
 
-var book = {
-    name:'Functional Javascript',
-    author:'Lwis Handshake',
-    publisher :'O\'Really',
-    page:259,
-    print:function(){
-        console.log(this.name,this.author)
-    }
+// var book = {
+//     name:'Functional Javascript',
+//     author:'Lwis Handshake',
+//     publisher :'O\'Really',
+//     page:259,
+//     print:function(){
+//         console.log(this.name,this.author)
+//     }
+// }
+
+// console.log(book)
+// book.print()
+
+// console.log('Book Name: '+ book.name);
+// console.log('Auhtor Name: ' + book['author'])// To access object property with bracket notation & property name must be in single or double quotes
+
+// // In Objects,properties could be created with dots
+// book.publishedYear = 2010;// Adding properties into book object
+
+// console.log('Published Year : ' + book.publishedYear)
+
+// // In Objects,properties could be created with bracket notation
+// book['price'] = 30;
+// console.log('Price: ' + book.price)
+
+// // Now book Object have 7 properties
+// //Print object with values method 1
+// console.log(book)
+
+// //Print object with values using for in loop
+// for (var props in book){
+//     console.log(props)
+//     console.log(props + ' = ' + book.props)//This will return undefined
+//     console.log(props + ' = ' + book[props])//With brakcet notations the problem is solved,so this is Method
+// }
+
+function Person(name,email) {
+    this.name = name;
+    this.email = email;
 }
 
-console.log(book)
-book.print()
+var p1 = new Person('Sakif Hridoy','sakifhridoy@mail.com');
 
-console.log('Book Name: '+ book.name);
-console.log('Auhtor Name: ' + book['author'])// To access object property with bracket notation & property name must be in single or double quotes
+console.log(p1.name);
+console.log(p1.email);
+console.log(p1);
 
-// In Objects,properties could be created with dots
-book.publishedYear = 2010;// Adding properties into book object
 
-console.log('Published Year : ' + book.publishedYear)
-
-// In Objects,properties could be created with bracket notation
-book['price'] = 30;
-console.log('Price: ' + book.price)
-
-// Now book Object have 7 properties
-//Print object with values method 1
-console.log(book)
-
-//Print object with values using for in loop
-for (var props in book){
-    console.log(props)
-    console.log(props + ' = ' + book.props)//This will return undefined
-    console.log(props + ' = ' + book[props])//With brakcet notations the problem is solved,so this is Method
-}
